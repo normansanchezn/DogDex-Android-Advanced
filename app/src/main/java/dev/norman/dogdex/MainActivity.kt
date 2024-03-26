db1539641e24d3ab3801053f891f417476cdbb6a
Norman Sánchez <normansanchezn@gmail.com>
2024-03-25 22:21:05 -0600

    [🎉] Project added

---
+++ app/src/main/java/dev/norman/dogdex/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.norman.dogdex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
